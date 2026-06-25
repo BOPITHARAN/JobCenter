c4bca74f12a58e19159b0c4121c596eb49103cc7
mohanapiriyan-05 <mohanapiriyan05oms@gmail.com>
2026-06-25 10:09:08 +0530

    fix api base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    // VITE_API_URL இல்லையென்றால், நேரடி Railway URL-ஐப் பயன்படுத்தவும்
-    baseURL: import.meta.env.VITE_API_URL || 'https://jobcenterback-production.up.railway.app', 
-    withCredentials: true 
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://jpbcenterback-production.up.railway.app",
+  withCredentials: true,
 });
 
 export default api;
